7db425348fc2ea5dce7e4daf31e722e3d8982b71
aostrovska <anastasija.ostrovska25@gmail.com>
2021-02-26 20:17:04 +0200

    new types:Student&Stuff

--- baza_of_stud_2j.go
+++ baza_of_stud_2j.go
@@ -1,28 +1,68 @@
 package main
 
-import "fmt"
-import "encoding/json"
-import "os"
-import "bufio"
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"os"
+)
 
 type Action struct {
-	Action string `json:"action"`
+	Action  string `json:"action"`
 	ObjName string `json:"object"`
 }
 
 type Teacher struct {
-	ID string  `json:"id"`
-	Salary float64 `json:"salary"`
-	Subject string `json:"subject"`
+	ID        string   `json:"id"`
+	Salary    float64  `json:"salary"`
+	Subject   string   `json:"subject"`
 	Classroom []string `json:"classroom"`
-	Person struct {
-		Name string `json:"name"`
-		Surname string `json:"surname"`
+	Person    struct {
+		Name         string `json:"name"`
+		Surname      string `json:"surname"`
 		PersonalCode string `json:"personalCode"`
 	} `json:"person"`
 }
 
-var obj_sl []GeneralObject 
+type Stuff struct {
+	ID        string   `json:"id"`
+	Salary    float64  `json:"salary"`
+	Classroom []string `json:"classroom"`
+	Person    struct {
+		Name         string `json:"name"`
+		Surname      string `json:"surname"`
+		PersonalCode string `json:"personalCode"`
+	} `json:"person"`
+}
+
+type Student struct {
+	ID     string  `json:"id"`
+	Salary float64 `json:"salary"`
+	Class  string  `json:"class"`
+	Person struct {
+		Name         string `json:"name"`
+		Surname      string `json:"surname"`
+		PersonalCode string `json:"personalCode"`
+	} `json:"person"`
+}
+
+var obj_sl []GeneralObject
+
+func GetId(o GeneralObject) string {
+	switch o.(type) {
+	case *Teacher:
+		t := o.(Teacher)
+		return t.ID
+	case *Stuff:
+		s := o.(Stuff)
+		return s.ID
+	case *Student:
+		st := o.(Student)
+		return st.ID
+
+	}
+	return ""
+}
 
 func (t Teacher) GetCreateAction() DefinedAction {
 	return &CreateTeacher{}
@@ -35,6 +75,32 @@ func (t Teacher) GetReadAction() DefinedAction {
 }
 func (t Teacher) GetDeleteAction() DefinedAction {
 	return &DeleteTeacher{}
+}
+
+func (s Stuff) GetCreateAction() DefinedAction {
+	return &CreateStuff{}
+}
+func (s Stuff) GetUpdateAction() DefinedAction {
+	return &UpdateStuff{}
+}
+func (s Stuff) GetReadAction() DefinedAction {
+	return &ReadStuff{}
+}
+func (s Stuff) GetDeleteAction() DefinedAction {
+	return &DeleteStuff{}
+}
+
+func (st Student) GetCreateAction() DefinedAction {
+	return &CreateStudent{}
+}
+func (st Student) GetUpdateAction() DefinedAction {
+	return &UpdateStudent{}
+}
+func (st Student) GetReadAction() DefinedAction {
+	return &ReadStudent{}
+}
+func (st Student) GetDeleteAction() DefinedAction {
+	return &DeleteStudent{}
 }
 
 type DefinedAction interface {
@@ -52,7 +118,8 @@ type GeneralObject interface {
 type CreateTeacher struct {
 	T Teacher `json:"data"`
 }
-func (action *CreateTeacher) GetFromJSON (rawData []byte) {
+
+func (action *CreateTeacher) GetFromJSON(rawData []byte) {
 	err := json.Unmarshal(rawData, action)
 	if err != nil {
 		fmt.Println(err)
@@ -66,7 +133,8 @@ func (action CreateTeacher) Process() {
 type UpdateTeacher struct {
 	T Teacher `json:"data"`
 }
-func (action *UpdateTeacher) GetFromJSON (rawData []byte) {
+
+func (action *UpdateTeacher) GetFromJSON(rawData []byte) {
 	err := json.Unmarshal(rawData, action)
 	if err != nil {
 		fmt.Println(err)
@@ -74,7 +142,11 @@ func (action *UpdateTeacher) GetFromJSON (rawData []byte) {
 	}
 }
 func (action UpdateTeacher) Process() {
-	
+	for i := 0; i < len(obj_sl); i++ {
+		if action.T.ID == GetId(obj_sl[i]) {
+			obj_sl[i] = action.T
+		}
+	}
 }
 
 type ReadTeacher struct {
@@ -82,7 +154,8 @@ type ReadTeacher struct {
 		ID string `json:"id"`
 	} `json:"data"`
 }
-func (action *ReadTeacher) GetFromJSON (rawData []byte) {
+
+func (action *ReadTeacher) GetFromJSON(rawData []byte) {
 	err := json.Unmarshal(rawData, action)
 	if err != nil {
 		fmt.Println(err)
@@ -90,19 +163,11 @@ func (action *ReadTeacher) GetFromJSON (rawData []byte) {
 	}
 }
 func (action ReadTeacher) Process() {
-	 for i := 0; i < len(obj_sl); i++{
-		 if action.Data.ID == GetId(obj_sl[i]){
-			 fmt.Println(obj_sl[i])
+	for i := 0; i < len(obj_sl); i++ {
+		if action.Data.ID == GetId(obj_sl[i]) {
+			fmt.Println(obj_sl[i])
 		}
-	} 
-}
-
-func GetId(o GeneralObject)string{
-		switch o.(type){
-			case *Teacher: return o.ID
-	
-		}
-		return o.ID
+	}
 }
 
 type DeleteTeacher struct {
@@ -110,7 +175,8 @@ type DeleteTeacher struct {
 		ID string `json:"id"`
 	} `json:"data"`
 }
-func (action *DeleteTeacher) GetFromJSON (rawData []byte) {
+
+func (action *DeleteTeacher) GetFromJSON(rawData []byte) {
 	err := json.Unmarshal(rawData, action)
 	if err != nil {
 		fmt.Println(err)
@@ -118,40 +184,200 @@ func (action *DeleteTeacher) GetFromJSON (rawData []byte) {
 	}
 }
 func (action DeleteTeacher) Process() {
-	for i := 0; i < len(obj_sl); i++{
-		if GetId(obj_sl[i]) == action.Data.ID{
-			obj_sl[i]= nil 
+	for i := 0; i < len(obj_sl); i++ {
+		if GetId(obj_sl[i]) == action.Data.ID {
+			obj_sl[i] = nil
 		}
 	}
 }
 
-func main() {
-file, _ := os.Open("data.json")
-scanner := bufio.NewScanner(file)
-
-for scanner.Scan() {
-  var act Action
-  err := json.Unmarshal([]byte(scanner.Text()), &act)
-  
-	var obj GeneralObject
-	switch act.ObjName {
-	case "Teacher":
-		obj = &Teacher{}
-	}
-	var toDo DefinedAction
-	switch act.Action {
-	case "create":
-		toDo = obj.GetCreateAction()
-		toDo.Process()
-	case "update":
-		toDo = obj.GetUpdateAction()
-		toDo.Process()
-	case "read":
-		toDo = obj.GetReadAction()
-		toDo.Process()
-	}
-//	toDo.GetFromJSON(file)
-	
-	toDo.Process()
+type CreateStuff struct {
+	S Stuff `json:"data"`
 }
+
+func (action *CreateStuff) GetFromJSON(rawData []byte) {
+	err := json.Unmarshal(rawData, action)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+}
+func (action CreateStuff) Process() {
+	obj_sl = append(obj_sl, action.S)
+}
+
+type UpdateStuff struct {
+	S Stuff `json:"data"`
+}
+
+func (action *UpdateStuff) GetFromJSON(rawData []byte) {
+	err := json.Unmarshal(rawData, action)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+}
+func (action UpdateStuff) Process() {
+	for i := 0; i < len(obj_sl); i++ {
+		if action.S.ID == GetId(obj_sl[i]) {
+			obj_sl[i] = action.S
+		}
+	}
+}
+
+type ReadStuff struct {
+	Data struct {
+		ID string `json:"id"`
+	} `json:"data"`
+}
+
+func (action *ReadStuff) GetFromJSON(rawData []byte) {
+	err := json.Unmarshal(rawData, action)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+}
+func (action ReadStuff) Process() {
+	for i := 0; i < len(obj_sl); i++ {
+		if action.Data.ID == GetId(obj_sl[i]) {
+			fmt.Println(obj_sl[i])
+		}
+	}
+}
+
+type DeleteStuff struct {
+	Data struct {
+		ID string `json:"id"`
+	} `json:"data"`
+}
+
+func (action *DeleteStuff) GetFromJSON(rawData []byte) {
+	err := json.Unmarshal(rawData, action)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+}
+func (action DeleteStuff) Process() {
+	for i := 0; i < len(obj_sl); i++ {
+		if GetId(obj_sl[i]) == action.Data.ID {
+			obj_sl[i] = nil
+		}
+	}
+}
+
+type CreateStudent struct {
+	St Student `json:"data"`
+}
+
+func (action *CreateStudent) GetFromJSON(rawData []byte) {
+	err := json.Unmarshal(rawData, action)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+}
+func (action CreateStudent) Process() {
+	obj_sl = append(obj_sl, action.St)
+}
+
+type UpdateStudent struct {
+	St Student `json:"data"`
+}
+
+func (action *UpdateStudent) GetFromJSON(rawData []byte) {
+	err := json.Unmarshal(rawData, action)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+}
+func (action UpdateStudent) Process() {
+	for i := 0; i < len(obj_sl); i++ {
+		if action.St.ID == GetId(obj_sl[i]) {
+			obj_sl[i] = action.St
+		}
+	}
+}
+
+type ReadStudent struct {
+	Data struct {
+		ID string `json:"id"`
+	} `json:"data"`
+}
+
+func (action *ReadStudent) GetFromJSON(rawData []byte) {
+	err := json.Unmarshal(rawData, action)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+}
+func (action ReadStudent) Process() {
+	for i := 0; i < len(obj_sl); i++ {
+		if action.Data.ID == GetId(obj_sl[i]) {
+			fmt.Println(obj_sl[i])
+		}
+	}
+}
+
+type DeleteStudent struct {
+	Data struct {
+		ID string `json:"id"`
+	} `json:"data"`
+}
+
+func (action *DeleteStudent) GetFromJSON(rawData []byte) {
+	err := json.Unmarshal(rawData, action)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+}
+func (action DeleteStudent) Process() {
+	for i := 0; i < len(obj_sl); i++ {
+		if GetId(obj_sl[i]) == action.Data.ID {
+			obj_sl[i] = nil
+		}
+	}
+}
+func main() {
+	file, _ := os.Open("data.json")
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		var act Action
+		err := json.Unmarshal([]byte(scanner.Text()), &act)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		var obj GeneralObject
+		switch act.ObjName {
+		case "Teacher":
+			obj = &Teacher{}
+		case "Stuff":
+			obj = &Stuff{}
+		case "Student":
+			obj = &Student{}
+
+		}
+		var toDo DefinedAction
+		switch act.Action {
+		case "create":
+			toDo = obj.GetCreateAction()
+			toDo.Process()
+		case "update":
+			toDo = obj.GetUpdateAction()
+			toDo.Process()
+		case "read":
+			toDo = obj.GetReadAction()
+			toDo.Process()
+		}
+		//toDo.GetFromJSON(file)
+
+		toDo.Process()
+	}
 }
